0213b86b283d1858b968f9f8b8824a24f92d3f5a
yuhongxing <xkoven@gmail.com>
2022-07-02 16:33:08 -0700

    init

--- scripts/debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /viscam/u/koven/anaconda3/bin/activate
-conda activate votenet-3090
-cd /viscam/u/koven/2022_EON/EON-VoteNet
-LOGDIR='logs/debug'
-mkdir -p $LOGDIR
-EXPDIR=$LOGDIR'/00_original_dataset_ours'
-CUDA_VISIBLE_DEVICES=0 PYTHONBREAKPOINT='IPython.embed' python train.py --log_dir $EXPDIR \
---dataset_folder 'scan2cad_ex1.0_nosample'
--- scripts/exp220702_debug/01_generate_dataset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /viscam/u/koven/anaconda3/bin/activate
-conda activate EON-VoteNet
-cd /viscam/u/koven/2022_EON/EON-VoteNet/scan2cad
-python generate_cad2align_and_rot.py
